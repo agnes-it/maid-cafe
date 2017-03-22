6abdcb765a3f5d4b303636453f8b70b3b0aabc04
StefanYohansson <sy.fen0@gmail.com>
2017-03-21 21:49:49 -0300

    Clock logic.

--- kitchen/src/components/clock/index.js
+++ kitchen/src/components/clock/index.js
@@ -2,9 +2,36 @@ import { h, Component } from 'preact';
 import style from './style';
 
 export default class Clock extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      hours: '',
+      minutes: '',
+      seconds: ''
+    };
+
+    this.tick = this.tick.bind(this);
+  }
+
+  componentDidMount() {
+    this.tick();
+  }
+
+  tick() {
+    const date = new Date;
+    this.setState({
+      ...this.state,
+      hours: date.getHours(),
+      minutes: date.getMinutes(),
+      seconds: date.getSeconds()
+    });
+  }
+
   render() {
+    setTimeout(this.tick, 100);
     return (
-      <h1>10:30:25</h1>
+      <h1>{this.state.hours}:{this.state.minutes}:{this.state.seconds}</h1>
     );
   }
 }
