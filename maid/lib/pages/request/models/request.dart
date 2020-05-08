ff295483b220dd024d4b8cb6395df324e5719552
StefanYohansson <sy.fen0@gmail.com>
2020-05-08 18:41:07 +0100

    Move additionalInfo from requestmenu into request

--- maid/lib/pages/request/models/request.dart
+++ maid/lib/pages/request/models/request.dart
@@ -8,11 +8,12 @@ class Request extends Equatable {
   final String maid;
   final String table;
   final List<RequestMenu> menus;
+  final String additionalInfo;
 
-  const Request({this.id, this.maid, this.table, this.menus, this.order});
+  const Request({this.id, this.maid, this.table, this.menus, this.order, this.additionalInfo});
 
   @override
-  List<Object> get props => [id, maid, table, menus, order];
+  List<Object> get props => [id, maid, table, menus, order, additionalInfo];
 
   @override
   String toString() => 'Menu { id: $id }';
--- maid/lib/pages/request/models/request_menu.dart
+++ maid/lib/pages/request/models/request_menu.dart
@@ -3,12 +3,11 @@ import 'package:equatable/equatable.dart';
 class RequestMenu extends Equatable {
   final int menu;
   final int amount;
-  final String additionalInfo;
 
-  const RequestMenu({this.menu, this.amount, this.additionalInfo});
+  const RequestMenu({this.menu, this.amount});
 
   @override
-  List<Object> get props => [menu, amount, additionalInfo];
+  List<Object> get props => [menu, amount];
 
   @override
   String toString() => 'RequestMenu { menu: $menu, amount: $amount }';
