e81e6b946b56528156d38951116c61ad738721cc
Cirios Santhiago <cirios.santhiago@outlook.com>
2020-05-14 09:14:56 -0300

    Add django admin status

--- admin/maidadmin/urls.py
+++ admin/maidadmin/urls.py
@@ -17,12 +17,16 @@ from django.urls import include, path
 from django.contrib import admin
 from cafe.api import api_urls
 from rest_framework.authtoken.views import obtain_auth_token
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 
 urlpatterns = [
     path('api/', include(api_urls)),
     path('admin/', admin.site.urls),
+    path('admin_tools_stats/', include('admin_tools_stats.urls')),
     path('api-token-auth/', obtain_auth_token),
     path('api-auth/',
     include('rest_framework.urls', namespace='rest_framework')),
-]
+] + static(settings.STATIC_URL, document_root=settings.MEDIA_ROOT)
